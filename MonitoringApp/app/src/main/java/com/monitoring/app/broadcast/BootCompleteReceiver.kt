da5a09d42ad0b5708eaad9911f1a32e87b2dfa7f
kenwu565657 <kenwu565656@icloud.com>
2025-07-14 23:30:25 +0800

    add broadcast receiver

---
+++ MonitoringApp/app/src/main/java/com/monitoring/app/broadcast/BootCompleteReceiver.kt
@@ -0,0 +1,12 @@
+package com.monitoring.app.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootCompleteReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
