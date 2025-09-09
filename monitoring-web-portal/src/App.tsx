aac187d73f83298c73c67adb1cd5c904dcc19669
kenwu565657 <kenwu565656@icloud.com>
2025-09-10 03:33:12 +0800

    init frontend admin web portal react ts vite project

---
+++ monitoring-web-portal/src/App.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+function App() {
+    const router = createBrowserRouter([]);
+    return <RouterProvider router={router} />;
+}
+
+export default App
